3bcf75dc996f998a4c431f6c5409d6cee7eb0081
Joax <36775053+Joexder@users.noreply.github.com>
2026-01-28 23:53:35 -0400

    Adornando el temporizador

--- minijuego/main.js
+++ minijuego/main.js
@@ -1,19 +1,19 @@
+// Variables de elementos HTML
 let pantallainicial = document.querySelector(".pantallaini");
-
 let globo = document.getElementById("globo"); 
-let mensaje = document.getElementById("mensaje"); 
-let size = 100; 
+let mensaje = document.getElementById("mensaje");
 
-// tamaño inicial 
-let maxSize = 300; 
-// tamaño máximo antes de explotar 
-let tiempoLimite = 10000;
-// 10 segundos 
-let startTime = Date.now(); 
+// Variables del juego
+let size = 100; // tamaño inicial 
+let maxSize = 300; // tamaño máximo antes de explotar 
+let tiempoLimite = 10000; // 10 segundos
+let startTime = Date.now(); // tiempo de inicio del juego
 
+// Evento para ocultar la pantalla inicial al hacer clic 
 pantallainicial.addEventListener("click", () => {
     pantallainicial.classList.add("ocultarinicio");
 });
+
 
 document.body.onkeydown = function(e) { 
     console.log(startTime);
